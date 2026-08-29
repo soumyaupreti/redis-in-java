749d4ac8b5a44519234a35d2ccc1dadf82d3cbcb
Soumya Upreti <soumyaupreti7@gmail.com>
2026-08-29 20:55:37 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,8 @@ public class Main {
          serverSocket.setReuseAddress(true);
          // Wait for connection from client.
          clientSocket = serverSocket.accept();
+           OutputStream out = clientSocket.getOutputStream();
+           outputStream.write("+PONG\r\n".getBytes());
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
