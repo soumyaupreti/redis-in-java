7f94c28c601da1bc95b4d3d0ff960386463e606f
Soumya Upreti <soumyaupreti7@gmail.com>
2026-08-30 20:05:29 +0530

    *wy1*

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,17 +14,12 @@ public class Main {
        Socket clientSocket = null;
        int port = 6379;
        try {
-         serverSocket = new ServerSocket(port);
+         while(true){serverSocket = new ServerSocket(port);
          // Since the tester restarts your program quite often, setting SO_REUSEADDR
          // ensures that we don't run into 'Address already in use' errors
          serverSocket.setReuseAddress(true);
          clientSocket = serverSocket.accept();
-         clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-         InputStream inputStream = clientSocket.getInputStream();
-         byte[] buffer = new byte[1024];
-         int bytesRead;
-         while ((bytesRead = inputStream.read(buffer)) != -1) {
-           clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+         clientSocket.getOutputStream().write("+PONG\r\n".getBytes());}
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
