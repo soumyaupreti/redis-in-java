e84e2cd7ca0612980ae6f8dcf7d4c45aa994c67c
Soumya Upreti <soumyaupreti7@gmail.com>
2026-09-11 21:02:54 +0530

    resp deserializer

--- src/main/java/Components/TcpServer.java
+++ src/main/java/Components/TcpServer.java
@@ -62,6 +62,8 @@ public class TcpServer {
         while(client.socket.isConnected()){
            byte[] buffer = new byte[client.socket.getReceiveBufferSize()];
            int bytesRead = client.inputStream.read(buffer);
+           System.out.println(bytesRead);
+           
            if (bytesRead>0) {
                // bytes parsing into strings
                List<String[]> res = respSerializer.deserialize(buffer);
