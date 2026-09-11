e83c5b3353e3da85de612b8c218b1f0ffc46bb57
Soumya Upreti <soumyaupreti7@gmail.com>
2026-09-11 20:59:59 +0530

    resp deserializer

--- src/main/java/Components/TcpServer.java
+++ src/main/java/Components/TcpServer.java
@@ -1,5 +1,14 @@
 package Components;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired; 
 @Component
 public class TcpServer {
@@ -8,6 +17,81 @@ public class TcpServer {
     public void startServer(){
         respSerializer.printWorking();
     }
+    ServerSocket serverSocket = null;
+       Socket clientSocket = null;
+       int port = 6379;
+       try {
+          serverSocket = new ServerSocket(port);
+    
+         serverSocket.setReuseAddress(true);
+         int id = 0;
+         while(true){
+               clientSocket = serverSocket.accept();
+               id++;
+               Socket finalClientSocket = clientSocket;
+               
+               InputStream inputStream = clientSocket.getInputStream();
+               OutputStream outputStream = clientSocket.getOutputStream();
+               Client client = new Client(finalClientSocket, inputStream, outputStream, id );
+               CompletableFuture.runAsync(() -> {
+                   try {
+                       handleClient(client);
+                   } catch (IOException e) {
+                       throw new RuntimeException(e);
+              }
+               }
+             };
+
+           
+             
+           }
+           
+       } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+       } finally {
+         try {
+           if (clientSocket != null) {
+             clientSocket.close();
+           }
+         } catch (IOException e) {
+           System.out.println("IOException: " + e.getMessage());
+         }
+    private static void handleClients(Client client) throws IOException {
+
+        System.out.println("==========================================================================")
+        while(client.socket.isConnected()){
+           byte[] buffer = new byte[client.socket.getReceiveBufferSize()];
+           int bytesRead = client.inputStream.read(buffer);
+           if (bytesRead>0) {
+               // bytes parsing into strings
+               List<String[]> res = respSerializer.deserialize(buffer);
+               for(String[] s : res){
+                   System.out.println();
+                   for(String ss : s){
+                       System.out.print(ss + " ");
+                   }
+               }
+           }
+        }
+
+        // Scanner sc = new Scanner(client.inputStream);
+       
+        // while(sc.hasNextLine()){
+        //      String nextLine = sc.next();
+        //      System.out.println(nextLine);
+        //      if(nextLine.contains(*ECHO*)){
+        //       outputStream.write(*+PONG\r\n*.getBytes());
+        //      }
+            //  if(nextLine.contains(*ECHO*)){
+            //   String respHeader = sc.nextLine();
+            //   String respBody = sc.nextLine();
+            //   String response = respHeader + *\r\n* + respBody + *\r\n*;
+
+            //   outputStream.write(response.getBytes());
+            //  }
+            //  }
+         
+         }
 
 
     }
